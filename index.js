781a8b8ae935bf193c407c1130019fa0eec38477
ProfessionalLime1144 <102875598+ProfessionalLime1144@users.noreply.github.com>
2023-11-20 17:41:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,47 +9,44 @@ app.listen(3000 || process.env.PORT, () => {
 });
 
 app.get("/", async (req, res) => {
-      // const url = "https://ocw.mit.edu/ans7870/9/9.00SC/MIT9_00SCF11_text.pdf";
-      // const url = "https://pdfco-test-files.s3.us-west-2.amazonaws.com/document-parser/sample-invoice.pdf"
-    let url = req.get("url");
-    if (!url.startsWith("https:")) {
-      url = "https:" + url;
-    };
-  
-    try {
-      // Fetch the PDF file as binary data
-      const response = await fetch(url);
-      
-      if (!response.ok) {
-        console.log(url);
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      const data = await response.arrayBuffer();
-
-      // Convert binary data to text and then embed it
-      const parsedData = await PdfParse(data);
-
-      const text = parsedData.text;
-      const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 500,
-        chunkOverlap: 100
-      });
-
-      const docs = await splitter.createDocuments([text]);
-      let arrayDocs = [];
-
-      for (const doc of docs) {
-        arrayDocs.push(doc.pageContent);
-      }
-
-      arrayDocs = arrayDocs.slice(0, 1500);
-      res.json({ arrayDocs });
-      
-      // res.send(JSON.stringify(arrayDocs));
-      console.log("Documents Created");
-      
-    } catch(err) {
-      res.json({ error: err });
-      console.log(err);
+  console.log("Creating Documents");
+  let url = req.get("url");
+  if (!url.startsWith("https:")) {
+    url = "https:" + url;
+  };
+  try {
+    // Fetch the PDF file as binary data
+    const response = await fetch(url);
+    
+    if (!response.ok) {
+      console.log(url);
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+    const data = await response.arrayBuffer();
+
+    // Convert binary data to text and then embed it
+    const parsedData = await PdfParse(data);
+    
+    const text = parsedData.text;
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 500,
+      chunkOverlap: 100
+    });
+
+    const docs = await splitter.createDocuments([text]);
+    let arrayDocs = [];
+
+    for (const doc of docs) {
+      arrayDocs.push(doc.pageContent);
+    };
+    arrayDocs = arrayDocs.slice(0, 1500);
+    res.json({ arrayDocs });
+    
+    console.log("Documents Created");
+    // res.send(JSON.stringify(arrayDocs));
+      
+  } catch(err) {
+    res.json({ error: err });
+    console.log(err);
+  };
 });
